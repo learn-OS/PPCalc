8c1df204d691d62f99b000d771286077d9fd68dc
Carles Company Soler <carles@company.cat>
2014-03-18 07:36:45 +0100

    Tracked OldPreferencesActivity

--- PPCalc/src/main/java/cat/company/ppcalc/preferences/OldPreferencesActivity.java
+++ PPCalc/src/main/java/cat/company/ppcalc/preferences/OldPreferencesActivity.java
@@ -3,6 +3,8 @@ package cat.company.ppcalc.preferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 import cat.company.ppcalc.R;
 
 /**
@@ -14,5 +16,17 @@ public class OldPreferencesActivity extends PreferenceActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EasyTracker.getInstance(this).activityStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EasyTracker.getInstance(this).activityStop(this);
     }
 }

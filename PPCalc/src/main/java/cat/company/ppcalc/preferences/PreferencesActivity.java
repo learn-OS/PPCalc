613702385c669c446466848813dbb6083f2f5437
Carles Company Soler <carles@company.cat>
2014-08-09 10:25:45 +0200

    Removed unused using.

--- PPCalc/src/main/java/cat/company/ppcalc/preferences/PreferencesActivity.java
+++ PPCalc/src/main/java/cat/company/ppcalc/preferences/PreferencesActivity.java
@@ -1,7 +1,6 @@
 package cat.company.ppcalc.preferences;
 
 import android.annotation.TargetApi;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
